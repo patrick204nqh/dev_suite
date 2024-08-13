515d0f72920b56a46ce9ddc365335124c17f9c39
Huy Nguyen <patrick204nqh@gmail.com>
2024-08-13 16:55:53 +0700

    chore: Add logger, error handler modules and color to CLI

--- lib/dev_suite.rb
+++ lib/dev_suite.rb
@@ -2,11 +2,12 @@
 
 module DevSuite
   require_relative "dev_suite/version"
-  require_relative "dev_suite/cli"
+  require_relative "dev_suite/logger"
+  require_relative "dev_suite/error_handler"
   require_relative "dev_suite/utils"
+  require_relative "dev_suite/cli"
   require_relative "dev_suite/performance"
   require_relative "dev_suite/directory_tree"
 
   class Error < StandardError; end
-  # Your code goes here...
 end
